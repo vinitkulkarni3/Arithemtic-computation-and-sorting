dc2f2dd1a1c44fbf6650bc6e51af2b67b11dda82
vinitkulkarni3 <vinitkulkarni4321@gmail.com>
2020-11-16 12:22:39 +0530

    Added: Case 2 (a*b+c) is Added

--- gitprogram1.sh
+++ gitprogram1.sh
@@ -5,13 +5,17 @@ read num1
 read num2
 read num3
 
-echo "1.(a+b*c)"
+echo "1.(a+b*c) 2.(a*b+c)"
 read -p "Enter the option: " option
 
 case $option in
 	1)answer1=`echo - | awk '{print '$num1' + '$num2'}'`
 	answer1=$(($answer1*$num3))
 	echo "Answer = "$answer1
+	;;
+	2)answer2=$(($num1*$num2))
+	answer2=$(($answer2+$num3))
+	echo "Answer ="$answer2
 	;;
 	*)echo "Invalid option"
 	;;
