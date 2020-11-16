38ac3c3a003b13d835844bd41ef503f6201c270c
vinitkulkarni3 <vinitkulkarni4321@gmail.com>
2020-11-16 12:28:28 +0530

    Added: case 3 (c+a/b) is Added

--- gitprogram1.sh
+++ gitprogram1.sh
@@ -5,7 +5,7 @@ read num1
 read num2
 read num3
 
-echo "1.(a+b*c) 2.(a*b+c)"
+echo "1.(a+b*c) 2.(a*b+c) 3.(c+a/b)"
 read -p "Enter the option: " option
 
 case $option in
@@ -16,6 +16,10 @@ case $option in
 	2)answer2=$(($num1*$num2))
 	answer2=$(($answer2+$num3))
 	echo "Answer ="$answer2
+	;;
+	3)answer3=$(($num3+$num1))
+	answer3=`echo - | awk '{print '$answer3' / '$num2'}'`
+	echo "Answer = "$answer3
 	;;
 	*)echo "Invalid option"
 	;;
