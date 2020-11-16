db9090527c494da1b651766c264c0a380d10eb28
vinitkulkarni3 <vinitkulkarni4321@gmail.com>
2020-11-16 16:31:17 +0530

    Added: Results to Dictionary

--- gitprogram1.sh
+++ gitprogram1.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+declare -A arr
+k=0;
+n=1;
 echo "Enter three numbers:"
 read num1
 read num2
 read num3
 
+while [ $n -gt 0 ]
+do
 echo "1.(a+b*c) 2.(a*b+c) 3.(c+a/b) 4.(a%b+c)"
 read -p "Enter the option: " option
 
@@ -12,20 +17,33 @@ case $option in
 	1)answer1=`echo - | awk '{print '$num1' + '$num2'}'`
 	answer1=$(($answer1*$num3))
 	echo "Answer = "$answer1
+	arr[$k]=$answer1
+	((k++))
 	;;
 	2)answer2=$(($num1*$num2))
 	answer2=$(($answer2+$num3))
-	echo "Answer ="$answer2
+	echo "Answer = "$answer2
+	arr[$k]=$answer2
+	((k++))
 	;;
 	3)answer3=$(($num3+$num1))
 	answer3=`echo - | awk '{print '$answer3' / '$num2'}'`
 	echo "Answer = "$answer3
+	arr[$k]=$answer3
+	((k++))
 	;;
 	4)answer4=`echo - | awk '{print '$num1' % '$num2'}'`
 	answer4=$(($answer4+$num3))
 	echo "Answer = "$answer4
+	arr[$k]=$answer4
+        ((k++))
 	;;
 	*)echo "Invalid option"
 	;;
 esac
+	echo "press 0 to exit press 1 to continue"
+	read e
+	n=$e
+done
 
+echo "Dictionary elements are : "${arr[@]}
